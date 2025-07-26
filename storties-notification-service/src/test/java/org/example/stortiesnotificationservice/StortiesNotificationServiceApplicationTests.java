be770a8bdbbd18753f9151216fa5e3c54ee60b05
김경민 <kkm07133@naver.com>
2025-07-26 14:41:45 +0900

    feat: (#1) 프로젝트 세팅

---
+++ storties-notification-service/src/test/java/org/example/stortiesnotificationservice/StortiesNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stortiesnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StortiesNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
